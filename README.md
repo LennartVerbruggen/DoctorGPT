64bf627a228c4cdcb17958eeb0999b15b2cd30f2
Lennart Verbruggen <lennart.verbruggen@student.ucll.be>
2023-10-26 11:34:47 +0200

    Log in werkt met hardcoded authenticatie

--- README.md
+++ README.md
@@ -3,3 +3,10 @@
 Extensions needed for project to run:
 - pip install Flask
 - pip install bootstrap-flask
+- pip install Flask-Login
+
+
+Aanpassingen aan packages:
+- In flaks_login/utils.py verwijder volgende imports:
+    - from werkzeug.urls import url_encode
+    - from werkzeug.urls import url_decode
--- webapp/app.py
+++ webapp/app.py
@@ -1,16 +1,56 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
 from flask_bootstrap import Bootstrap4
+from flask_login import LoginManager, UserMixin, current_user, login_user, login_required, logout_user
 
 app = Flask(__name__)
+app.secret_key = 'doctorgpt'
 bootstrap = Bootstrap4
+login_manager = LoginManager()
+login_manager.init_app(app)
+
+
+# Hardcoded users.
+users = {'test1@t.be': {'password': 'test1'}, 'zieke@t.be': {'password': 'griep'}}
+
+# Create a User class to represent users.
+class User(UserMixin):
+    pass
+
+@login_manager.user_loader
+def load_user(user_id):
+    if user_id in users:
+        user = User()
+        user.id = user_id
+        return user
+    return None
 
 @app.route("/")
 def index():
     return render_template("index.html")
 
-@app.route("/login")
+@app.route("/login", methods=['GET', 'POST'])
 def login():
+    if request.method == 'POST':
+        email = request.form['email']
+        password = request.form['password']
+        if email in users and users[email]['password'] == password:
+            user = User()
+            user.id = email
+            login_user(user)
+            return redirect(url_for('protected'))
     return render_template("login.html")
+
+@app.route('/logout')
+@login_required
+def logout():
+    logout_user()
+    return redirect(url_for('login'))
+
+@app.route('/protected')
+@login_required
+def protected():
+    return 'You are logged in as ' + UserMixin.get_id(current_user)
+
 
 if __name__ == "__main__":
     app.run(debug=True)
